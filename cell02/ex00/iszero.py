c15923ce830036ab9c0007313b0399fe0851b498
tyayod <Mamonkey243@gmail.com>
2024-10-08 12:24:05 +0700

    Delete cell02/ex00 directory

--- cell02/ex00/iszero.py
+++
@@ -1,9 +0,0 @@
-number = input("please enter number : ")
-number = int(number)
-
-
-if number == 0:
-   print("This number is different from zero")
-else :
-   print("This number is equal to zero")
- 
