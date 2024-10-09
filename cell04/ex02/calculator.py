3f98021b90449e9deaa31f5eb70a412b2d9d83dc
tyayod <Mamonkey243@gmail.com>
2024-10-09 12:49:51 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+first = int(input("Give me the first number :"))
+second = int(input("Give me the second number :"))
+
+def calculator():
+    print("Thank you")
+    print(f"{first} + {second} = {first+second} ")
+    print(f"{first} - {second} = {first-second} ")
+    print(f"{first} / {second} = {first/second} ")
+    print(f"{first} * {second} = {first*second} ")
+
+calculator()
