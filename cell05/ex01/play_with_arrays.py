4908bbb2c2755109dd125f56131999920a296638
tyayod <Mamonkey243@gmail.com>
2024-10-09 13:52:48 +0700

    Add files via upload

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def main():
+   Original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+   new_array = [num + 2 for num in Original_array]
+   print("Original array :", Original_array)
+   print("new array :", new_array)
+
+main()
